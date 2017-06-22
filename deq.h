a6d8ad9068ca656ea17d4671f2a8b63b31f8a63c
loserock <loserock@gmail.com>
2017-06-22 15:09:14 +0200

    default class implementation and tests dummy size() and empty() implementation

--- deq.h
+++ deq.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <type_traits>
+#include <array>
+#include <vector>
+
 // feature list:
 //  - push_back, pop_back: uses the end's flag position
 //  - push_front, pop_front: uses the begin's flag position
@@ -7,4 +11,32 @@
 //  - begin and end shall works as invariants (like std lib)
 //  - use a predefined box size for optimization
 
+template <typename T, size_t N = 16, typename B = std::vector<std::array<T, N> *>>
+class deq
+{
+  public:
+    bool empty() const noexcept;
+    size_t size() const noexcept;
 
+  private:
+    static const size_t ImplSize = N;
+
+    using impl_t = B;
+    using buffer_t = typename std::remove_pointer<typename B::value_type>::type;
+
+    impl_t m_v;
+    size_t m_beg;
+    size_t m_end;
+};
+
+template <typename T, size_t N, typename B>
+bool deq<T, N, B>::empty() const noexcept
+{
+    return true;
+}
+
+template <typename T, size_t N, typename B>
+size_t deq<T, N, B>::size() const noexcept
+{
+    return 0;
+}
--- deq_test.cpp
+++ deq_test.cpp
@@ -1,11 +1,15 @@
 #include <gtest/gtest.h>
 #include "deq.h"
 
+TEST(DeqTestCreate, Trivial)
+{
+    deq<int> di;
 
+    EXPECT_TRUE(di.empty());
+    EXPECT_EQ(0, di.size());
+}
 
-
-
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
