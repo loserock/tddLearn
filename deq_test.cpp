a862c9ed1100995550f0efc0017e402946e1f993
loserock <loserock@gmail.com>
2017-06-23 10:28:49 +0200

    deq test regroupping

--- deq_test.cpp
+++ deq_test.cpp
@@ -28,87 +28,6 @@ TEST(DeqTestPushBack, AfterEmpty)
     EXPECT_EQ(1, di.size());
 }
 
-TEST(DeqTestPushBack, AddFew)
-{
-    deq<int> di;
-    size_t s = 10; ///< number of inserted elements
-    fillBack(di, s);
-
-    EXPECT_FALSE(di.empty());
-    EXPECT_EQ(s, di.size());
-}
-
-TEST(DeqTestPushBack, AddMany)
-{
-    deq<int> di;
-
-    const auto s = 10;  ///< number of inserted elements
-    const auto cn = 42; ///< number of cycles
-    for (size_t i = 0; i < cn; ++i)
-    {
-        fillBack(di, s);
-
-        ASSERT_FALSE(di.empty());
-        ASSERT_EQ((i + 1) * s, di.size());
-    }
-}
-
-TEST(DeqTestPopBack, OnlyOne)
-{
-    deq<int> di;
-
-    di.push_back(42);
-    di.pop_back();
-
-    EXPECT_TRUE(di.empty());
-    EXPECT_EQ(0, di.size());
-}
-
-TEST(DeqTestPopBack, Many)
-{
-    deq<int> di;
-
-    const auto s = 150;
-    fillBack(di, s);
-
-    for (size_t i = s; i > 1; --i)
-    {
-        di.pop_back();
-
-        ASSERT_FALSE(di.empty());
-        ASSERT_EQ(i - 1, di.size());
-    }
-    di.pop_back();
-
-    EXPECT_TRUE(di.empty());
-    EXPECT_EQ(0, di.size());
-}
-
-TEST(DeqTestPushPopBack, Many)
-{
-    const auto nPush1 = 150;
-    const auto nPush2 = 99;
-    const auto nPop1 = 123;
-    const auto nPop2 = 86;
-
-    deq<int> di;
-
-    fillBack(di, nPush1);
-    for (size_t i = nPop1; i > 0; --i)
-    {
-        di.pop_back();
-    }
-
-    fillBack(di, nPush2);
-    for (size_t i = nPop2; i > 0; --i)
-    {
-        di.pop_back();
-    }
-
-    EXPECT_FALSE(di.empty());
-    EXPECT_EQ(nPush1 + nPush2 - nPop1 - nPop2, di.size());
-}
-
 TEST(DeqTestIndexOperator, AfterPushBack)
 {
     deq<int> di;
@@ -237,6 +156,87 @@ TEST(DeqTestCopy, SelfAssignAfterPushPop)
     {
         ASSERT_EQ(100 + i, di[i]);
     }
+}
+
+TEST(DeqTestPushBack, AddFew)
+{
+    deq<int> di;
+    size_t s = 10; ///< number of inserted elements
+    fillBack(di, s);
+
+    EXPECT_FALSE(di.empty());
+    EXPECT_EQ(s, di.size());
+}
+
+TEST(DeqTestPushBack, AddMany)
+{
+    deq<int> di;
+
+    const auto s = 10;  ///< number of inserted elements
+    const auto cn = 42; ///< number of cycles
+    for (size_t i = 0; i < cn; ++i)
+    {
+        fillBack(di, s);
+
+        ASSERT_FALSE(di.empty());
+        ASSERT_EQ((i + 1) * s, di.size());
+    }
+}
+
+TEST(DeqTestPopBack, OnlyOne)
+{
+    deq<int> di;
+
+    di.push_back(42);
+    di.pop_back();
+
+    EXPECT_TRUE(di.empty());
+    EXPECT_EQ(0, di.size());
+}
+
+TEST(DeqTestPopBack, Many)
+{
+    deq<int> di;
+
+    const auto s = 150;
+    fillBack(di, s);
+
+    for (size_t i = s; i > 1; --i)
+    {
+        di.pop_back();
+
+        ASSERT_FALSE(di.empty());
+        ASSERT_EQ(i - 1, di.size());
+    }
+    di.pop_back();
+
+    EXPECT_TRUE(di.empty());
+    EXPECT_EQ(0, di.size());
+}
+
+TEST(DeqTestPushPopBack, Many)
+{
+    const auto nPush1 = 150;
+    const auto nPush2 = 99;
+    const auto nPop1 = 123;
+    const auto nPop2 = 86;
+
+    deq<int> di;
+
+    fillBack(di, nPush1);
+    for (size_t i = nPop1; i > 0; --i)
+    {
+        di.pop_back();
+    }
+
+    fillBack(di, nPush2);
+    for (size_t i = nPop2; i > 0; --i)
+    {
+        di.pop_back();
+    }
+
+    EXPECT_FALSE(di.empty());
+    EXPECT_EQ(nPush1 + nPush2 - nPop1 - nPop2, di.size());
 }
 
 int main(int argc, char **argv)
